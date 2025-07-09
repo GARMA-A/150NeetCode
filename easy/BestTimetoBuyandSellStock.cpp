7a15b17527831e92d88bf0bd81e2096c99804d06
Girgis <girgisemad74@gmail.com>
2025-07-09 05:18:15 +0300

    solve one more problem

---
+++ easy/BestTimetoBuyandSellStock.cpp
@@ -0,0 +1,30 @@
+#include <climits>
+#include <iostream>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int maxProfit(vector<int> &prices) {
+
+    // Input: prices = [10,1,5,6,7,1]
+    //
+    // Output: 6
+
+    int l = 0;
+    int mx_profit = 0;
+    for (int i = 1; i < prices.size(); i++) {
+      if (prices[l] <= prices[i]) {
+        mx_profit = max(mx_profit, prices[i] - prices[l]);
+
+      } else {
+        l = i;
+      }
+    }
+    return mx_profit;
+  }
+};
+
+int main() {}
