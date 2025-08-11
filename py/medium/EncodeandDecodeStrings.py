ea65c05117c45d53e89658964d4a4b30656c9936
Girgis <girgisemad74@gmail.com>
2025-08-11 08:33:07 +0300

    solve one easy using py

---
+++ py/medium/EncodeandDecodeStrings.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        """
+        Input: ["neet","code","love","you"]
+        1n2e1t$1c1o1d1e$1l1o1v1e
+        """
+        for s in strs:
+
+
+
+        return ""
+
+    def decode(self, s: str) -> List[str]:
+        return []
