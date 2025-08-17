e499e72ce391eb81db56a9c47a838dd8113ebf85
Girgis <girgisemad74@gmail.com>
2025-08-17 13:13:29 +0300

    solving more

---
+++ py/medium/MergeTwoSortedLinkedLists.py
@@ -0,0 +1,19 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def mergeTwoLists(
+        self, list1: Optional[ListNode], list2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        if not list1:
+            return list2
+        if not list2:
+            return list2
+
+        pass
