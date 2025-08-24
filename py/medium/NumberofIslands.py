99f2d2af5aab01ad8cc681ae97155b941cab8a92
Girgis <girgisemad74@gmail.com>
2025-08-25 02:13:41 +0300

    solve two medium problem

---
+++ py/medium/NumberofIslands.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def __init__(self) -> None:
+        self.UP = [-1,0],
+        self.DOWN=[1,0]
+        self.RIGHT = [0,1],
+        self.LEFT = [0,-1]
+        
+
+    def numIslands(self, grid: List[List[str]]) -> int:
+        
+        return -1
+    
+    def dfs(grid:List[List[str]]):
+
+
+        
+
