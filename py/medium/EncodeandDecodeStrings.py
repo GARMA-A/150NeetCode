7fbd72b12b12f5c05f28041eff7d2133cc0191f6
Girgis <girgisemad74@gmail.com>
2025-09-02 07:59:59 +0300

    solve two medium problems

--- py/medium/EncodeandDecodeStrings.py
+++ py/medium/EncodeandDecodeStrings.py
@@ -6,13 +6,27 @@ class Solution:
     def encode(self, strs: List[str]) -> str:
         """
         Input: ["neet","code","love","you"]
-        1n2e1t$1c1o1d1e$1l1o1v1e
+        neet:;code:;love:;you:;
         """
+        encoded = ""
         for s in strs:
+            encoded += str(len(s)) + "#" + s
 
-
-
-        return ""
+        return encoded
 
     def decode(self, s: str) -> List[str]:
-        return []
+        current_str, size_str, size_num, ans = "", "", 0, []
+        i = 0
+        while i < len(s):
+            current_str = ""
+            size_str = ""
+            while s[i] != "#":
+                size_str += s[i]
+                i += 1
+            i += 1
+            size_num = int(size_str)
+            current_str = s[i : i + size_num]
+            ans.append(current_str)
+            i += size_num
+
+        return ans
