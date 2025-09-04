b5c2284c425f2d5dff3ae007f4048e700ac13af2
Girgis <girgisemad74@gmail.com>
2025-09-04 10:01:07 +0300

    solve two problems medium

---
+++ py/medium/LongestConsecutiveSequence.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        st = set(nums)
+        ans = 0
+
+        for num in st:
+            if (num - 1) not in st:
+                length = 0
+                while (num + length) in st:
+                    length += 1
+                    ans = max(ans, length)
+
+        return ans
