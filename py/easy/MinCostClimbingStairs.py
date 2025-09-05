b38ea239dfff1a105b108bca3b66b1188e80ec7d
Girgis <girgisemad74@gmail.com>
2025-09-05 12:24:49 +0300

    solve three easy

---
+++ py/easy/MinCostClimbingStairs.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        memo = [-1] * len(cost)
+
+        def dfs(idx: int):
+            if idx >= len(cost):
+                return 0
+            if memo[idx] != -1:
+                return memo[idx]
+            memo[idx] = cost[idx] + min(dfs(idx + 1), dfs(idx + 2))
+            return memo[idx]
+
+        return min(dfs(0), dfs(1))
+
+
+s = Solution()
+
+print(s.minCostClimbingStairs([1, 2, 3]))
