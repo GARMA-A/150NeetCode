f4fdfdfd6b2ca2bc72d73c8bcc79b3556c5492fd
Girgis <girgisemad74@gmail.com>
2025-09-06 21:11:22 +0300

    done

---
+++ py/medium/MaximumSubarray.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        ans, curSum = nums[0], 0
+        for num in nums:
+            if curSum < 0:
+                curSum = 0
+            curSum += num
+            ans = max(ans, curSum)
+        return ans
