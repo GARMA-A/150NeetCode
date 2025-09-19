28bdfb3900138f80b3d215598fdab668d8fb14b2
Girgis <girgisemad74@gmail.com>
2025-09-19 04:50:29 +0300

    solve two easy

--- py/medium/IslandsandTreasure.py
+++ py/medium/IslandsandTreasure.py
@@ -1,30 +1,7 @@
-from typing import List, Mapping
+from typing import List
 
 
 class Solution:
     def islandsAndTreasure(self, grid: List[List[int]]) -> None:
-        island, water, treasure = 2147483647, -1, 0
 
-        def DFS(row: int, col: int, visited) -> int:
-            if (
-                row < 0
-                or row >= len(grid)
-                or col < 0
-                or col >= len(grid[0])
-                or visited[str(row) + str(col)]
-            ):
-                return 0
-            elif grid[row][col] == water:
-                return island
-            elif grid[row][col] == treasure:
-                return 0
-            visited[str(row) + str(col)] = True
-            grid[row][col] = 1 + min(
-                DFS(row - 1, col, visited),
-                DFS(row + 1, col, visited),
-                DFS(row, col + 1, visited),
-                DFS(row, col - 1, visited),
-            )
-            return 0
-
-        DFS(0, 0, {})
+        return None
