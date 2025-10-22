70bcaac9dde40e346cc8cb50397a84fb2738dcd0
Girgis <girgisemad74@gmail.com>
2025-10-22 04:05:13 +0300

    implement the DFS BFS algorithsm

---
+++ py/graphs/dfs_bfs_002.py
@@ -0,0 +1,89 @@
+from collections import deque
+from typing import List, Mapping
+
+adj_list : Mapping[str , List[str] ]= {}
+adj_list["A"] = ["B" ,"C"]
+adj_list["B"] = ["A","D"]
+adj_list["C"]  = []
+adj_list["D"] = ["F"]
+adj_list["E"] = []
+adj_list["F"] = ["E"]
+
+def DFS(startNode:str):
+    stack = [startNode]
+    visited = {}
+    while stack:
+        top = stack.pop()
+        print(top , end=" ")
+        visited[top] = True
+        for node in adj_list[top]:
+            if node not in visited:
+                visited[node] = True
+                stack.append(node)
+# DFS("A")
+# print()
+
+def BFS(startNode:str):
+    q = deque(startNode)
+    visited = {}
+    while q:
+        top = q.popleft()
+        visited[top] =True
+        print(top , end=" ")
+        for node in adj_list[top]:
+            if node not in visited:
+                visited[node] = True
+                q.append(node)
+BFS("A")
+
+# A B C D E F
+twoDArray =[
+[0, 1, 1, 0, 0, 0],
+[1, 0, 0, 1, 0, 0],
+[0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 1],
+[0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 1, 0]
+ ]
+
+
+
+def DFS2(startNode : str):
+    stack = [startNode]
+    visited = set()
+    while stack:
+        top= stack.pop()
+        visited.add(top)
+        print(top ,end=" ")
+        for idx,val in enumerate(twoDArray[ord(top)-ord("A")]):
+            ch = chr(ord("A") + idx)
+            if val and ch not in visited:
+                visited.add(str(ord("A")+idx))
+                stack.append(ch)
+print()
+# DFS2("A")
+
+
+
+
+def BFS2(startNode :str):
+    q = deque(startNode)
+    visited = set()
+    while q:
+        top = q.popleft()
+        visited.add(top)
+        print(top ,  end=" ")
+        for idx ,val in enumerate(twoDArray[ord(top)-ord("A")]):
+            ch = chr(ord("A")+idx)
+            if val and ch not in visited:
+                visited.add(ch)
+                q.append(ch)
+
+BFS2("A")
+
+
+
+
+
+
+
---
+++ stonycodes70proplems/medium/LongestFibonacciSubarray.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n < 3:
+            return n
+
+        max_len = 2
+        curr_len = 2
+        for i in range(2, n):
+            if nums[i] == nums[i-1] + nums[i-2]:
+                curr_len += 1
+                max_len = max(max_len, curr_len)
+            else:
+                curr_len = 2
+        return max_len
+
+
